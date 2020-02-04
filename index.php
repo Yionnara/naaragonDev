dd85e78f75aba77990c2687e5d1fc1bc4fe4be47
Aragon Nathan <nathan.aragon@etu.u-bordeaux.fr>
2020-02-04 09:17:55 +0100

    First commit navbar + icon + header

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<?php include __DIR__ . "/templates/header.html"; ?>
+<body>
+<?php include __DIR__ . "/templates/navbar.php"; ?>
+<h1 class="text-xl-center">Voici la page d'accueil du portfolio de Nathan ARAGON</h1>
+</body>
+<?php include __DIR__ . "/templates/footer.html"; ?>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<nav class="navbar navbar-expand-sm bg-secondary navbar-dark">
+    <a class="nav-link" href="#"><img style="width: 40px;height: 40px" src="/img/logoBlanc.png" alt="Logo"/></a>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="#">Présentation</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Diplômes</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Compétences</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Expérience</a>
+        </li>
+    </ul>
+</nav>
